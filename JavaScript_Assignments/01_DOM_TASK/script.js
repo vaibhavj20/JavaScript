b1901a6b404647ecd2bd264fda6311896bd76b5c
Vaibhav <vaibhavjam86@gmail.com>
2024-02-20 18:54:17 +0530

    Done

---
+++ JavaScript_Assignments/01_DOM_TASK/script.js
@@ -0,0 +1,14 @@
+let mainHeader=document.getElementById('main-heading');
+mainHeader.textContent='Fruit World';
+
+mainHeader.style.color='orange';
+
+let content=document.getElementById('header');
+content.style.backgroundColor='green';
+content.style.borderBottom=' 3px solid orange';
+
+let basketHeading=document.getElementById('basket-heading');
+basketHeading.style.color='green';
+
+let thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
